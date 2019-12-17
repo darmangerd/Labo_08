4642f5c0291a555917756f0b546c3c497fc63896
Melvin <merk.melvin@gmail.com>
2019-12-17 16:11:58 +0100

    fix com

--- affichage.cpp
+++ affichage.cpp
@@ -1,16 +1,19 @@
 /*
  -----------------------------------------------------------------------------------
  Laboratoire : 08
- Fichier     : main.cpp
+ Fichier     : affichage.cpp
  Auteur(s)   : Melvin Merk, David Darmanger, Rébecca Tevaearai
  Date        : 17.12.2019
+
  But         :  Utilisation de tableaux (type C) bi-dimensionnels ou vecteurs
                 Utilisation de type enuméré
                 Conception et mise en oeuvre de fonctions
                 Approfondissement de la notion de tests et décision
                 Manipulation de chaines de caractères
                 Mise en oeuvre de la compilation séparée en plusieurs fichiers.
+
  Remarque(s) : Ne fonctionne que avec un tablier de taille strictement inférieur à 10
+
  Compilateur : MinGW-g++ 6.3.0
  -----------------------------------------------------------------------------------
  */
@@ -26,11 +29,11 @@ const string CASE_LIBRE = "..";
 const int LARGEUR_COLONNE = 3;
 
 /**
- * @brief       Fonction qui parcourt le tableau "tablier" et affiche
- *              chaque case selon son état: vide, avec une bille ou libre.
+ * @brief                   Fonction qui parcourt le tableau "tablier" et affiche
+ *                          chaque case selon son état: vide, avec une bille ou libre.
  * 
  * 
- * @param       Le tableau à deux dimensions "tablier".
+ * @param[in] tablier       Le tableau à deux dimensions "tablier".
  */        
 void afficheTablier(EtatCase tablier[][TAILLE_TABLIER]) {
   
@@ -54,10 +57,10 @@ void afficheTablier(EtatCase tablier[][TAILLE_TABLIER]) {
 }
 
 /**
- * @brief       Fonction qui affiche les déplacements possibles.
+ * @brief                  Fonction qui affiche les déplacements possibles.
  * 
  * 
- * @param       Le tableau à deux dimensions "tablier". 
+ * @param[in] tablier      Le tableau à deux dimensions "tablier".
  */   
 void afficheAide(EtatCase tablier[][TAILLE_TABLIER]) {
   vector<string> move = allPossibleMoves(tablier);
@@ -71,7 +74,7 @@ void afficheAide(EtatCase tablier[][TAILLE_TABLIER]) {
  * @brief       Fonction qui affiche un message de fin différent selon le résultat final.
  * 
  * 
- * @param       Le nombre de billes restantes et un boolean "centre" qui, dans le cas gagnant
+ * @param[in]       Le nombre de billes restantes et un boolean "centre" qui, dans le cas gagnant
  *              (une seule bille restante), indique si la dernière bille est au centre du tablier.
  *  
  */   
--- reglesDuJeu.h
+++ reglesDuJeu.h
@@ -1,7 +1,7 @@
 /*
  -----------------------------------------------------------------------------------
  Laboratoire : 08
- Fichier     : main.cpp
+ Fichier     : reglesDuJeu.h
  Auteur(s)   : Melvin Merk, David Darmanger, Rébecca Tevaearai
  Date        : 17.12.2019
 
@@ -34,26 +34,26 @@ const int NB_BILLE_BIEN_MAX = 5;
 enum class EtatCase{VIDE, BILLE, LIBRE};
 
 /**
-\brief Permet de voir tous les mouvements possible (help)
-\param[in] tablier tableau contenant les différents états des cases
-\return retourne tous les mouvements possibles
+@brief                      Permet de voir tous les mouvements possible (help)
+@param[in] tablier          tableau contenant les différents états des cases, le tableau n'est pas modifié
+@return                     tous les mouvements possibles
 */
 std::vector<std::string> allPossibleMoves(EtatCase tablier[][TAILLE_TABLIER]);
 
 /**
-\brief Permet de déplacer une bille sur le plateau
-\param[in] tablier tableau contenant les différents états des cases
-\param[in] userinput string qui contient le déplacement choisi par l'utilisateur
-\return retourne vrai si l'index se trouve dans les intervalles valides, false s'il ne s'y trouve pas
+@brief                      Permet de déplacer une bille sur le plateau
+@param[in] tablier          tableau contenant les différents états des cases, le tableau est modifié si le mouvement est valide
+@param[in] userinput        string qui contient le déplacement choisi par l'utilisateur
+@return                     vrai si le déplacement de l'utilisateur est valide
 */
 bool moveBille(EtatCase tablier[][TAILLE_TABLIER], const std::string &userinput);
 
 /**
-\brief Permet de vérifier si la partie est fini
-\param[in] tablier tableau contenant les différents états des cases
-\param[in] billeRestante entier qui représente le nombre de bille restante
-\param[in] centre booléen qui est à vrai lorsque la dernière bille restante est au centre
-\return retourne vrai s'il n'y a plus de mouvement possible
+@brief                      Permet de vérifier si la partie est fini
+@param[in] tablier          tableau contenant les différents états des cases, le tableau n'est pas modifié
+@param[in] billeRestante    entier qui représente le nombre de bille restante
+@param[in] centre           booléen qui est à vrai lorsque la dernière bille restante est au centre
+@return                     vrai s'il n'y a plus de mouvement possible
 */
 bool checkFinished(EtatCase tablier[][TAILLE_TABLIER], int &billeRestante, bool &centre);
 
